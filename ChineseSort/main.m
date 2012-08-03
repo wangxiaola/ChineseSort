c1cdff8318ddd43011ea786325f92aa9639372cd
Bill <bill@BilldeiMac.local>
2012-08-03 12:45:30 +0800

    Initial Commit

---
+++ ChineseSort/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ChineseSort
+//
+//  Created by Bill on 12-8-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
